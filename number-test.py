02c107086c904a0a802b3e413c192744c6840e79
Tran Dinh Dung <trandungtk321@gmail.com>
2025-12-19 23:50:21 +0700

    00:23:38 - Dữ liệu dạng số và các hàm toán học

---
+++ number-test.py
@@ -0,0 +1,23 @@
+import math
+from math import ceil
+
+print(2)
+
+print(3.1234)
+
+print(-3.2134)
+
+print(2 * (3 + 4.5))
+
+print(5 % 2)
+
+number = -2
+print(str(number) + " la so nguoi yeu ma toi tung co.")
+print(abs(number))
+print(pow(number, 2))
+print(max(1, 2))
+print(min(1, 2))
+print(round(5.5))
+print(math.floor(5.5))
+print(math.ceil(5.5))
+print(math.sqrt(36))
