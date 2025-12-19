220db3ef2764ff6d355d0238e50e2f92f383b62d
Tran Dinh Dung <trandungtk321@gmail.com>
2025-12-20 00:09:22 +0700

    00:32:08 - Nhận dữ liệu người dùng nhập vào

---
+++ input-test.py
@@ -0,0 +1,4 @@
+name = input("Nhap ten cua ban tai day: ")
+age = input("Nhap tuoi cua ban tai day: ")
+print("Xin chao, " + name + ". Nam nay, ban " + age + " tuoi.")
+print(f"Xin chao, {name}. Nam nay, ban {age} tuoi.")
