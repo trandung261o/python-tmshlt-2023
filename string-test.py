405bca20a1497bc30b297122cf409f3a4ba274e7
Tran Dinh Dung <trandungtk321@gmail.com>
2025-12-19 23:33:21 +0700

    00:15:10 - Thao tác với string

---
+++ string-test.py
@@ -0,0 +1,11 @@
+channel_name = "To Mo sang \n\"hoc lap trinh\""
+
+print(channel_name.upper().isupper())
+
+print(len(channel_name))
+
+print(channel_name[0])
+
+print(channel_name.index("sang"))
+
+print(channel_name.replace("sang", "toi"))
