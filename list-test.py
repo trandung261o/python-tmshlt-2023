0a9342aa84b49d62c734aaacae46a2b899fd0f69
Tran Dinh Dung <trandungtk321@gmail.com>
2025-12-20 00:54:49 +0700

    00:45:11 - Dữ liệu dạng danh sách

---
+++ list-test.py
@@ -0,0 +1,9 @@
+teams = ["Barcelona", "Real Madrid", "Manchester United", "Liverpool", "Chelsea"]
+
+print(teams)
+print(teams[2])
+print(teams[-2])
+print(teams[1:4])
+
+teams[1] = "Arsenal"
+print(teams)
