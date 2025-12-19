63d08b94e69cbd631c2248d5b106e4577e3184ca
Tran Dinh Dung <trandungtk321@gmail.com>
2025-12-19 22:40:54 +0700

    00:06:59 - Biến và Kiểu dữ liệu

---
+++ variable.py
@@ -0,0 +1,10 @@
+character_name = "Nam"
+character_age = 50.1234
+is_handsome = False
+
+print(f"Day la mot cau chuyen nho ve anh {character_name} dap chai")
+
+character_name = "Huy"
+print(f"Nam nay anh {character_name} da {character_age} tuoi")
+print("Mac du vay, trong anh ay khoe manh hon")
+print(f"Nhung nguoi ban U{character_age} rat nhieu")
