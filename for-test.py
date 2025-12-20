0fd5f47da747509ca8df480fb6ebdedd54fa1581
Tran Dinh Dung <trandungtk321@gmail.com>
2025-12-20 18:06:19 +0700

    01:54:40 - Vòng lặp for

---
+++ for-test.py
@@ -0,0 +1,15 @@
+name = "Dung"
+
+for character in name:
+    print(character)
+
+teams = ["Barcelona", "Real Madrid", "Manchester United"]
+
+for name in teams:
+    print(name)
+
+for index in range(1, 5):
+    if index == 1:
+        print("Day la phan tu dau tien o trong mang")
+    else:
+        print(f"Day la phan tu o vi tri thu {index}")
