ec3450bfcb9c8b4e1655220f4444e760723e0f93
Tran Dinh Dung <trandungtk321@gmail.com>
2025-12-20 15:20:06 +0700

    01:23:18 - Các toán tử logic

---
+++ logic-test.py
@@ -0,0 +1,15 @@
+a = 200
+b = 400
+c = 100
+
+if (a > b) and (a > c):
+    print("a la so lon nhat")
+
+if (a == b) or (a == c):
+    print("co it nhat mot so bang gia tri voi a")
+
+d = True
+print(not d)
+
+if not a > b:
+    print("a khong lon hon b")
