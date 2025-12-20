37ed9673fae2668be5fd9caa890b3596ff3220b3
Tran Dinh Dung <trandungtk321@gmail.com>
2025-12-20 14:38:02 +0700

    01:05:20 - Thao tác với hàm

---
+++ function-test.py
@@ -0,0 +1,8 @@
+def say_hello(name, email):
+    print(f"Chao mung {name} den voi channel To Mo Sang hoc Lap trinh.")
+    print(f"Email cua ban la: {email}")
+
+print("Bat dau")
+say_hello("Dung", "dung@gmail.com")
+say_hello("Phong", "phong@gmail.com")
+print("Ket thuc")
